344be333494e9a14d44300e78702e005dd0e4d69
kamal14rajput <kamal14rajput@gmail.com>
2022-03-23 09:27:01 +0530

    ADDED: Vnet ,LB,autoscaling, VM, NSG

---
+++ Vm-Lb-Vnet/variables.tf
@@ -0,0 +1,9 @@
+variable "prefix" {
+    default = "rg"
+  description = "The prefix which should be used for all resources"
+}
+
+variable "location" {
+    default = "west us"
+  description = "The Azure Region in which all resources should be created."
+}
