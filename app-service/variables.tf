8b955d6b2da1162296a89872f3cb5e078c608302
kamal14rajput <kamal14rajput@gmail.com>
2022-03-23 09:42:08 +0530

    ADDED: app services

---
+++ app-service/variables.tf
@@ -0,0 +1,9 @@
+variable "prefix" {
+    default = "apps"
+  description = "prefix used for all resources."
+}
+
+variable "location" {
+    default = "west india"
+  description = "Azure location for app services"
+}
